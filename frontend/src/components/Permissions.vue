05c29404045b41ac9c8ca29653c233fb4d6a283a
Dmitriy Boger <kotvberloge@gmail.com>
2018-10-12 22:58:51 +0300

    Initial commit with basic frontend (#10)

---
+++ frontend/src/components/Permissions.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <template v-for="(block, key) in blocks">
+      <img
+        height="20px"
+        width="20px"
+        :src="`/static/${block.picture}`"
+      >
+      <span>{{ key }}</span>
+      <ul v-for="permission in block.permissions">
+        <li>{{ permission }}</li>
+      </ul>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Permissions',
+  props: {
+    blocks: Object
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    float: left;
+  }
+  span {
+    margin-left: 6px;
+  }
+</style>
